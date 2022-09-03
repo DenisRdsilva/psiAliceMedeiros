6fc3691421c9990a34554581a99fb7f027d40706
[Denis} <[den_ou@hotmail.com]>
2022-09-03 12:04:30 -0300

    outro teste

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Psicóloga Alice Medeiros',
       home: const MyHomePage(title: 'Site Alice'),
       debugShowCheckedModeBanner: false,
     );
