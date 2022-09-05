41b5e0dc639509be9e821d902cd5c50ae36de50c
[Denis} <[den_ou@hotmail.com]>
2022-09-05 03:28:48 -0300

    adapted to mobile

--- lib/main.dart
+++ lib/main.dart
@@ -67,11 +67,12 @@ class _MyHomePageState extends State<MyHomePage> {
         Container(height: 50, width: swidth, padding: EdgeInsets.only(right: 30),
           color:  Color.fromARGB(255, 70, 41, 90),
           child: Row(children: [
+            Spacer(flex:1),
             Container(width: 135, height: 50, alignment: Alignment.centerLeft, margin: EdgeInsets.only(left: swidth*0.02), child: TextButton(onPressed: () => scaffoldKey.currentState?.openDrawer(), 
               child: Row(children: [
                 Icon(Icons.menu, color: Colors.white, size: 25),
                 Text(' Sobre mim', style: GoogleFonts.raleway(color: Colors.white, fontSize: 17))]))),
-            Container(width: swidth*0.614, child: Spacer()),
+            Spacer(flex:150),
             Container(alignment: Alignment.centerRight, padding: EdgeInsets.only(left: swidth*0.025), child:
               TextButton (onPressed: _launchURL,
                 child: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
@@ -84,10 +85,12 @@ class _MyHomePageState extends State<MyHomePage> {
                   Container(child: Icon(FontAwesomeIcons.whatsapp, color: Colors.white, size: 18)),
                   Container(child: Text(' Converse conosco', style: TextStyle(color: Colors.white)))
             ]))),
+            Spacer(flex:1),
           ])),
         Center(child: Container(width: 700, height: 220, 
           child: Image.asset('assets/Logo2.png', fit: BoxFit.cover)
           )),
+        if (swidth>800)...[
         Container(width: swidth, height: 600, color: Color.fromARGB(255, 70, 41, 90), 
           margin: EdgeInsets.only(bottom: 120), child: Column(children: [
             Center(child: ImageSlideshow(                
@@ -114,6 +117,39 @@ class _MyHomePageState extends State<MyHomePage> {
                 ],
               ))  
             ])),
+        ]else...[
+          Container(width: swidth, height: 600, color: Color.fromARGB(255, 70, 41, 90), 
+          margin: EdgeInsets.only(bottom: 120), child: Column(children: [
+            Center(child: ImageSlideshow(                
+              width: swidth*0.95, /// Width of the [ImageSlideshow].
+              height: 600, /// Height of the [ImageSlideshow].
+              initialPage: 0, /// The page to show when first creating the [ImageSlideshow].
+              indicatorColor: Color.fromARGB(255, 70, 41, 90), /// The color to paint the indicator.
+              indicatorBackgroundColor: Colors.grey, 
+              autoPlayInterval: 3500,
+              isLoop: true,
+              indicatorRadius: 5,
+              onPageChanged: (value) {
+                print('Page changed: $value');
+              },
+              children: [
+                Row(children: [
+                  Container(width: swidth*(0.95), height: 600, child: Image.asset('assets/img1.jpeg', fit: BoxFit.cover)),
+                ]),
+                Row(children: [
+                  Container(width: swidth*(0.95), height: 600, child: Image.asset('assets/img4.jpeg', fit: BoxFit.cover))
+                ]),
+                Row(children: [
+                  Container(width: swidth*(0.95), height: 600, child: Image.asset('assets/img5.jpeg', fit: BoxFit.cover)),
+                  ]),
+                 Row(children: [
+                  Container(width: swidth*(0.95), height: 600, child: Image.asset('assets/img3.jpeg', fit: BoxFit.cover))
+                  ]) 
+                ],
+              ))  
+            ])),
+          ],
+          if (swidth>1000)...[
             Container(height: 350, width: swidth,
               child: Row(children: [
                 Container( height: 350, width: swidth*0.025, color: Color.fromARGB(255, 70, 41, 90)),
@@ -134,9 +170,32 @@ class _MyHomePageState extends State<MyHomePage> {
                 Container(width:swidth*0.19, height: 40, 
                   child: Text('Alice Medeiros', style: 
                     GoogleFonts.firaSans(color: Color.fromARGB(255, 18, 12, 81), fontSize: 24))),
-              ])),
+                ])),
               Container( height: 350, width: swidth*0.025, color: Color.fromARGB(255, 70, 41, 90)),
             ])),
+          ]else...[
+            Container(height: 650, width: swidth,
+              child: Column(children: [
+                Container(width: swidth, height: 140, alignment: Alignment.center, color: Color.fromARGB(255, 200, 180, 210),
+                    child: Center(child: TextButton(onPressed: () => scaffoldKey.currentState?.openDrawer(), child: 
+                      Container(width: swidth, height: 140, child: Row(children: [
+                        Container(width: 80, height: 80, margin: EdgeInsets.only(left: 30), child: Image.asset('assets/Logo1.png', fit: BoxFit.cover)),
+                        Container(width: 320, height: 140, alignment: Alignment.center, child: Text('Quem sou eu?', style: GoogleFonts.courierPrime(color: Color.fromARGB(255, 18, 12, 81), fontSize: 42)
+                      ))]
+                    ))
+                  ))),
+                Container(height: 293.1, width: swidth, color: Color.fromARGB(255, 70, 41, 90), padding: EdgeInsets.all(13), 
+                  child: CustomVideo()),
+              Container( height: 170, width: swidth, color:Color.fromARGB(255, 200, 180, 210), alignment: Alignment.center, child: Column(children: [
+                Container(margin: EdgeInsets.only(top: 30, left: 30), width:swidth, height: 80,
+                  child: Text('“Desbravando a dor e a delícia de tornar-se adulto.”', style: 
+                    GoogleFonts.firaSans(color: Color.fromARGB(255, 18, 12, 81), fontSize: 28, fontStyle: FontStyle.italic))),
+                Container(width:swidth, height: 30, margin: EdgeInsets.only(left: 30),
+                  child: Text('Alice Medeiros', style: 
+                    GoogleFonts.firaSans(color: Color.fromARGB(255, 18, 12, 81), fontSize: 24))),
+                ])),
+            ])),
+          ], if (swidth>1100)...[
           Container(margin: EdgeInsets.only(top: 50, bottom: 50), width: swidth, height: 790, child:
             Row(children: [
               Container(width: swidth*0.15, height: 640, alignment: Alignment.center, child: Column(children: [
@@ -157,7 +216,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       GoogleFonts.firaSans(color: Color.fromARGB(255, 18, 12, 81), fontSize: 19)),
                   )])
                 )),
-                Container(margin: EdgeInsets.only(bottom: 80), 
+                Container(margin: EdgeInsets.only(bottom: 70), 
                 width: swidth*0.7, height: 380,  decoration: BoxDecoration(color: Color.fromARGB(255, 210, 189, 221),
                 borderRadius: BorderRadius.circular(10), border: Border.all(width: 3, color: Color.fromARGB(255, 70, 41, 90))),
                 child: TextButton (onPressed: () {}, child:  Column(children: [
@@ -176,20 +235,54 @@ class _MyHomePageState extends State<MyHomePage> {
               )
             ),
           ]
-        )),
+        ))
+        ]else...[
+          Column(children: [
+              Container(margin: EdgeInsets.only(top: 70, bottom: 40), 
+                width: swidth*0.95, height: 330,  decoration: BoxDecoration(color: Color.fromARGB(255, 210, 189, 221),
+                borderRadius: BorderRadius.circular(10), border: Border.all(width: 3, color: Color.fromARGB(255, 70, 41, 90))),         
+                child: TextButton (onPressed: () {}, child:  Column(children: [
+                    Container(margin: EdgeInsets.only(top: 30, right: 20, left: 20), child: Text('SERVIÇOS', style: 
+                      GoogleFonts.courierPrime(color: Color.fromARGB(255, 18, 12, 81), fontSize: 28))),
+                    Container(margin: EdgeInsets.only(top: 25, right: 20, left: 20), child: Text('Psicoterapia individual nas modalidades presencial e online através de plataforma segura. Ambas semanais com duração de até 60 minutos.  No primeiro caso, nos encontramos pessoalmente em meu consultório. Já para o atendimento online, você pode escolher onde estará no momento da sessão, basta ter um computador, celular ou tablet com acesso à internet.', textAlign: TextAlign.justify, style: 
+                      GoogleFonts.firaSans(color: Color.fromARGB(255, 18, 12, 81), fontSize: 19)),
+                  )])
+                )),
+              Container(margin: EdgeInsets.only(bottom: 70), 
+                width: swidth*0.95, height: 700,  decoration: BoxDecoration(color: Color.fromARGB(255, 210, 189, 221),
+                borderRadius: BorderRadius.circular(10), border: Border.all(width: 3, color: Color.fromARGB(255, 70, 41, 90))),
+                child: TextButton (onPressed: () {}, child:  Column(children: [
+                    Container(margin: EdgeInsets.only(top: 30, right: 20, left: 20), child: Text('TEXTO EXPLICATIVO', style: 
+                      GoogleFonts.courierPrime(color: Color.fromARGB(255, 18, 12, 81), fontSize: 28))),
+                    Container(margin: EdgeInsets.only(top: 25, right: 20, left: 20), child: Text('Olhar para si mesmo é, muitas vezes, desafiador. Pode provocar medo, insegurança, aquele pensamento de “depois eu penso nisso, agora não”. Mas e quando aquilo que foi deixado para depois começa a nos atrapalhar, provocando ansiedade, tristeza e a sensação de que precisamos falar com alguém sobre aquele assunto? É a partir desse momento que a psicoterapia poderia ajudar. Pode parecer estranho falar sobre si com alguém que não conhecemos. Com o tempo, passamos a confiar no psicólogo que nos acompanha, nos sentimos à vontade para falar sobre os assuntos mais delicados, não nos sentimos julgados, mas acolhidos. Semana após semana percebemos as contribuições das sessões em nosso dia a dia, até que em conjunto com o nosso terapeuta decidimos que naquele momento já podemos seguir sem o acompanhamento profissional, que já conseguimos lidar melhor com os acontecimentos de nossas vidas. Deixamos de nos ver semanalmente, mas cientes de que podemos, a qualquer momento, escolher voltar. Essa é a beleza da psicoterapia.', textAlign: TextAlign.justify, style: 
+                      GoogleFonts.firaSans(color: Color.fromARGB(255, 18, 12, 81), fontSize: 19)),
+              )])))
+          ]),
+        ],
+        if (swidth>800)...[
         Container(height: 600, child: 
           Row(children: [
-            Container(width: swidth*0.5, height: 600, color: Colors.grey, child:
+            Container(width: swidth*0.5, height: 600, padding: EdgeInsets.all(5), color: Colors.grey, child:
               FlutterMap(
                 options: MapOptions(
-                    center: LatLng(-5.79448, -35.211),
-                    zoom: 13,
+                    center: LatLng(-5.820560, -35.209990),
+                    zoom: 17,
                 ),
                 layers: [
                     TileLayerOptions(
                         urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
                         userAgentPackageName: 'com.example.app',
                     ),
+                    MarkerLayerOptions(
+                      markers: [
+                          Marker(
+                            point: LatLng(-5.820560, -35.209990),
+                            width: 50,
+                            height: 50,
+                            builder: (context) => Icon(FontAwesomeIcons.locationDot, size: 30, color: Color.fromARGB(255, 70, 41, 90)),
+                          ),
+                      ],
+                  ),
                 ],
                 nonRotatedChildren: [
                     AttributionWidget.defaultWidget(
@@ -200,16 +293,18 @@ class _MyHomePageState extends State<MyHomePage> {
               )
             ),
             Column(children:[
-              Container(width: swidth*0.5, padding: EdgeInsets.only(left: 50, top: 5), height: 550, color: Color.fromARGB(255, 200, 180, 210), child:
+              Container(width: swidth*0.5, padding: EdgeInsets.only(top: 5), height: 550, color: Color.fromARGB(255, 200, 180, 210), child:
                 Column(children: [
                   Center(child: Container(width: 600, height: 200, 
                     child: Image.asset('assets/Logo2.png', fit: BoxFit.cover)
                   )),
-                  Container(margin: EdgeInsets.only(left: 10, top: 5, bottom: 15), alignment: Alignment.centerLeft, child: Text('Contatos para agendamento:', style: GoogleFonts.raleway(color: Color.fromARGB(255, 18, 12, 81), fontSize: 18, fontWeight: FontWeight.bold))),
-                  Container(margin: EdgeInsets.only(left: 10), alignment: Alignment.centerLeft, child: Text('Número: (84) 9123-3527', style: GoogleFonts.raleway(color: Color.fromARGB(255, 18, 12, 81), fontSize: 18, fontWeight: FontWeight.w600))),
-                  Container(margin: EdgeInsets.only(left: 10, top: 15, bottom: 15), alignment: Alignment.centerLeft, child: Text('E-mail: psicologaalicemedeiros@gmail.com', style: GoogleFonts.raleway(color: Color.fromARGB(255, 18, 12, 81), fontSize: 18, fontWeight: FontWeight.w600))),
+                  Container(margin: EdgeInsets.only(left: 55, top: 20, bottom: 15), alignment: Alignment.centerLeft, child: Text('> Endereço:', style: GoogleFonts.raleway(color: Color.fromARGB(255, 18, 12, 81), fontSize: 18, fontWeight: FontWeight.bold))),
+                  Container(margin: EdgeInsets.only(left: 70, bottom: 30), alignment: Alignment.centerLeft, child: Text('Avenida Amintas Barros, 3700, Corporate Tower Center, Torre B', style: GoogleFonts.raleway(color: Color.fromARGB(255, 18, 12, 81), fontSize: 18, fontWeight: FontWeight.w600))),
+                  Container(margin: EdgeInsets.only(left: 55, top: 5, bottom: 15), alignment: Alignment.centerLeft, child: Text('> Contatos para agendamento:', style: GoogleFonts.raleway(color: Color.fromARGB(255, 18, 12, 81), fontSize: 18, fontWeight: FontWeight.bold))),
+                  Container(margin: EdgeInsets.only(left: 70), alignment: Alignment.centerLeft, child: Text('Número: (84) 9123-3527', style: GoogleFonts.raleway(color: Color.fromARGB(255, 18, 12, 81), fontSize: 18, fontWeight: FontWeight.w600))),
+                  Container(margin: EdgeInsets.only(left: 70, top: 15, bottom: 15), alignment: Alignment.centerLeft, child: Text('E-mail: psicologaalicemedeiros@gmail.com', style: GoogleFonts.raleway(color: Color.fromARGB(255, 18, 12, 81), fontSize: 18, fontWeight: FontWeight.w600))),
                   Row(children: [
-                    Container(width: 30, height: 30, margin: EdgeInsets.only(left: 5), child:
+                    Container(width: 30, height: 30, margin: EdgeInsets.only(left: 65), child:
                       TextButton (onPressed: _launchURL, child:
                         Icon(FontAwesomeIcons.instagram, color: Color.fromARGB(255, 18, 12, 81), size: 25))),
                     Container(width: 30, height: 30, margin: EdgeInsets.only(left: 5), child:
@@ -221,7 +316,62 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Text('PsiAliceMedeiros © 2022 Designed by DSilva', style: TextStyle(color: Colors.white, fontSize: 16)))
             ])
           ])
-        )]
+        )]else...[
+          Column(children:[
+            Container(height: 200, child: 
+            Container(width: swidth, height: 600, padding: EdgeInsets.all(8), color: Colors.grey, child:
+              FlutterMap(
+                options: MapOptions(
+                    center: LatLng(-5.820560, -35.209990),
+                    zoom: 17,
+                ),
+                layers: [
+                    TileLayerOptions(
+                        urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        userAgentPackageName: 'com.example.app',
+                    ),
+                    MarkerLayerOptions(
+                      markers: [
+                          Marker(
+                            point: LatLng(-5.820560, -35.209990),
+                            width: 50,
+                            height: 50,
+                            builder: (context) => Icon(FontAwesomeIcons.locationDot, size: 30, color: Color.fromARGB(255, 70, 41, 90)),
+                          ),
+                      ],
+                  ),
+                ],
+                nonRotatedChildren: [
+                    AttributionWidget.defaultWidget(
+                        source: 'OpenStreetMap contributors',
+                        onSourceTapped: null,
+                    ),
+                ],
+              )
+            )),
+              Container(width: swidth, padding: EdgeInsets.only(top: 5), height: 550, color: Color.fromARGB(255, 200, 180, 210), child:
+                Column(children: [
+                  Center(child: Container(width: 600, height: 200, 
+                    child: Image.asset('assets/Logo2.png', fit: BoxFit.cover)
+                  )),
+                  Container(margin: EdgeInsets.only(left: 35, top: 20, bottom: 15), alignment: Alignment.centerLeft, child: Text('> Endereço:', style: GoogleFonts.raleway(color: Color.fromARGB(255, 18, 12, 81), fontSize: 18, fontWeight: FontWeight.bold))),
+                  Container(margin: EdgeInsets.only(left: 50, bottom: 30), alignment: Alignment.centerLeft, child: Text('Avenida Amintas Barros, 3700, Corporate Tower Center, Torre B', style: GoogleFonts.raleway(color: Color.fromARGB(255, 18, 12, 81), fontSize: 18, fontWeight: FontWeight.w600))),
+                  Container(margin: EdgeInsets.only(left: 35, top: 5, bottom: 15), alignment: Alignment.centerLeft, child: Text('> Contatos para agendamento:', style: GoogleFonts.raleway(color: Color.fromARGB(255, 18, 12, 81), fontSize: 18, fontWeight: FontWeight.bold))),
+                  Container(margin: EdgeInsets.only(left: 50), alignment: Alignment.centerLeft, child: Text('Número: (84) 9123-3527', style: GoogleFonts.raleway(color: Color.fromARGB(255, 18, 12, 81), fontSize: 18, fontWeight: FontWeight.w600))),
+                  Container(margin: EdgeInsets.only(left: 50, top: 15, bottom: 15), alignment: Alignment.centerLeft, child: Text('E-mail: psicologaalicemedeiros@gmail.com', style: GoogleFonts.raleway(color: Color.fromARGB(255, 18, 12, 81), fontSize: 18, fontWeight: FontWeight.w600))),
+                  Row(children: [
+                    Container(width: 30, height: 30, margin: EdgeInsets.only(left: 65), child:
+                      TextButton (onPressed: _launchURL, child:
+                        Icon(FontAwesomeIcons.instagram, color: Color.fromARGB(255, 18, 12, 81), size: 25))),
+                    Container(width: 30, height: 30, margin: EdgeInsets.only(left: 5), child:
+                      TextButton (onPressed: _launchURL1, child:   
+                        Icon(FontAwesomeIcons.whatsapp, color: Color.fromARGB(255, 18, 12, 81), size: 25)))],
+                  )]
+                )),
+              Container(width: swidth, height: 50, alignment: Alignment.center, color: Color.fromARGB(255, 70, 41, 90),
+                child: Text('PsiAliceMedeiros © 2022 Designed by DSilva', style: TextStyle(color: Colors.white, fontSize: 16)))
+            ])
+        ]],
       )), 
     );
   }
